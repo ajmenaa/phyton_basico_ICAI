3ed68a381c7ee846e5deecdc1a07778f84064b0f
LuisHM05 <165973650+LuisHM05@users.noreply.github.com>
2024-04-05 20:30:26 -0600

    Create Clase01.py

---
+++ Estudiantes/Luis_HM/Clase01.py
@@ -0,0 +1,38 @@
+""" Este es un comentario que puede 
+ocupar varias líneas, en donde se puede detallar el autor, que función cumple el código, 
+su alcance, etc """
+
+linea2 = '\n Programa Clase 1' # \n es un caracte especial que representa un salto de línea
+print('Bienvenidos al curso de Python' + linea2) #Esto es un mensaje de bienvenida
+
+print("Hola", linea2)
+
+
+
+#Asignación tipos de variables individual
+mensaje = 'Hola mundo desde python'
+numero = 2024
+version = 2.1 #variable tipo float o flotante
+visible = False
+
+#Asignación de tupla
+mensaje,numero, version, visible = 'Hola G1 Python',30,3.1, True
+
+
+print (mensaje, numero, version, visible)
+print(type(mensaje))
+print(type(numero))
+print(type(version))
+print(type(visible))
+
+#Cambio de inferencia de tipo
+mensaje = 20
+print (mensaje)
+print(type(mensaje))
+
+#leer datos -> input
+nombre = input('¿Cual es su nombre? ')
+edad = input('Ingrese su edad: ')
+print(f"Bienvenido: {nombre}, su edad es {edad}")
+
+print("Bienvenido", nombre, "su edad es", edad)
