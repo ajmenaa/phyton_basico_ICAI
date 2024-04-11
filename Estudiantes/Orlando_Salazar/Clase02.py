bfed684e67546b29e362b008dca07ae2b73c41c1
orsaac <orsaac@gmail.com>
2024-04-10 19:13:47 -0600

    Clase2

---
+++ Estudiantes/Orlando_Salazar/Clase02.py
@@ -0,0 +1,34 @@
+#Operador == (igualdad)
+x  = 10
+y = 10
+print(x == y) #True
+
+#Operador != (distinto a)
+x = 10
+y = 11
+print(x != y) #True
+
+#Operador < (Menor que)
+x = 5
+y = 10
+print(x < y) #True
+
+#Operador <= (menor o igual que)
+x = 10
+y = 10
+print(x <= y) #True
+
+#Operador > (Mayor que)
+x = 15
+y = 10
+print(x > y) #True
+
+#Operador >= (mayor o igual que)
+x = 10
+y = 10
+print(x <= y) #True
+
+a = 'Hola'
+b = 'hola'
+
+print(a == b)
--- Estudiantes/Orlando_Salazar/tempCodeRunnerFile.py
+++ Estudiantes/Orlando_Salazar/tempCodeRunnerFile.py
@@ -1,22 +1,4 @@
-#Time Conversiobr - www.101computing.net/time-conversion/ 
+a = 'Hola'
+b = 'hola'
 
-time = input("Enter a time in the hh:mm format (e.g 18:36): ")
-
-#Complete the code here...
-
-hours = int(time[:2])
-
-minutes = int(time[-2:])
-
-#print(hours,minutes)
-
-if hours == 0:
-    hours = 12
-    print(f"{hours}:{minutes} am")
-elif hours < 12:
-    print(f"{hours}:{minutes} am")
-elif hours == 12:
-    print(f"{hours}:{minutes} pm")
-elif hours < 24:
-    hours = hours - 12
-    print(f"{hours}:{minutes} pm")
+print(a == b)
