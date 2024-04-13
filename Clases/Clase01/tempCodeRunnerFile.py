75864a566137a107830d447c24607ff82f7f72bf
0110670639 <dbarrios@bp.fi.cr>
2024-04-12 18:15:53 -0600

    Daniela

---
+++ Clases/Clase01/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+#resta
+resta = 10 - 5
+print(f"La resta es: {resta}")
+
+# Multiplicación
+multiplicacion = 10 * 5
+print(f"La multiplicación es: {multiplicacion}")
+
+# División
+division = 10 / 5
+print(f"La división es: {division}")
