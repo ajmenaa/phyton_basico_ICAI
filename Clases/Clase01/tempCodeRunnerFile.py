22c8f7f08904a1d40028a3313d1207f9c4fff799
LuisHM05 <165973650+LuisHM05@users.noreply.github.com>
2024-04-12 18:16:00 -0600

    Actualización LEHM

--- Clases/Clase01/tempCodeRunnerFile.py
+++ Clases/Clase01/tempCodeRunnerFile.py
@@ -1,3 +1,11 @@
-x = 5
-y = 10
-print(x < y)
+#resta
+resta = 10 - 5
+print(f"La resta es: {resta}")
+
+# Multiplicación
+multiplicacion = 10 * 5
+print(f"La multiplicación es: {multiplicacion}")
+
+# División
+division = 10 / 5
+print(f"La división es: {division}")
---
+++ Estudiantes/johanhidalgo/Tarea1.py
@@ -0,0 +1,13 @@
+#base = float(input("Ingrese base: "))
+#altura = float(input("Ingrese altura: "))
+#resultado = base * altura
+#print(f"El área es {resultado}")
+
+#monto_dolares = float(input("Ingrese monto en dolares: "))
+#tipo_cambio = float(input("Tipo de cambio del día: "))
+#resultado = monto_dolares*tipo_cambio
+#print(f"El monto colonizado es {resultado}")
+
+grados_centigrados = float(input("Ingreses grados centigrados: "))
+resultado = (grados_centigrados* 9/5)+32
+print(f"Los grados Fahrenheit {resultado}")
