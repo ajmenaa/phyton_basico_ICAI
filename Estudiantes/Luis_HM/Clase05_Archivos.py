bdb2df26f6a1e16fdc101daae1a95c5afd0fdf5f
LuisHM05 <165973650+LuisHM05@users.noreply.github.com>
2024-04-18 11:43:46 -0600

    all changes class 5

--- Estudiantes/Luis_HM/Clase05_Archivos.py
+++ Estudiantes/Luis_HM/Clase05_Archivos.py
@@ -3,23 +3,35 @@ try:
     archivo = open("archivo.txt", "r")
 except Exception as e:
     print(f"Error al abrir el archivo: {e}")
-if archivo
+if archivo:
+    archivo.close
 """
+
+import os
 
 def leer_archivo(nombre_archivo):
     
     try:
-        with open(nombre_archivo, 'r') as archivo:
+        ruta = os.path.join("Estudiantes","Luis_HM",nombre_archivo)
+        with open(ruta, 'r') as archivo:
             contenido = archivo.read()
             return contenido
     except FileNotFoundError:
         return ''
 
-def agregar_texto():
-    pass
+def agregar_archivo(nombre_archivo, texto):
+    try:
+        ruta = os.path.join("Estudiantes","Luis_HM",nombre_archivo)
+        with open(ruta, "w") as archivo:
+        
 
 nombre_archivo = input("Ingrese el nombre del archivo a leer: ")
 texto_archivo = input("Ingrese el mensaje: ")
 
 contenido_leido = leer_archivo(nombre_archivo)
-print(contenido_leido)
+
+if not contenido_leido:
+    print('Archivo no encontrado')
+else:
+    print(f"El contenido del archivo {nombre_archivo} es: ")
+    print (contenido_leido)
