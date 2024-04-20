5123955c3856aeb8d8bfef57801cfecdc6c419be
orsaac <orsaac@gmail.com>
2024-04-20 17:50:48 -0600

    LaClinica

--- Estudiantes/Orlando_Salazar/ProyectoCorto/LaClinica.py
+++ Estudiantes/Orlando_Salazar/ProyectoCorto/LaClinica.py
@@ -1,13 +1,28 @@
 import json
 
 archivo = open("clinica.json", "r")
+#archivo = open("C:\Users\salazaor\Documents\GitHub\phyton_basico_ICAI\Estudiantes\Orlando_Salazar\ProyectoCorto\clinica.json", "r")
 contenido = archivo.read()
 
 archivo.close()
 
 datos = json.loads(contenido)
 
-print(datos[0][5])
+#print(datos)
+
+encabezados = ['Identificacion','Telefono','Provincia','Canton','Enfermedad','Medicamento']
+
+lista_diccionarios = []
+
+datos_diccionario = {}
+
+for lineas in range(len(datos)):
+    for encabezado in range(len(encabezados)):
+        datos_diccionario[encabezados[encabezado]] = datos[lineas][encabezado]
+        lista_diccionarios.append(datos_diccionario)
+
+print(lista_diccionarios)
+
 
 
         
